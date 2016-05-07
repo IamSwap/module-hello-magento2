36906eab7aea1e583ed31ac17dca315e6dcb6891
Swapnil Bhavsar <swapnil_bhavsar@hotmail.com>
2016-05-07 19:54:36 +0530

    First commit.

---
+++ app/code/Swapnil/HelloMagento/Block/Main.php
@@ -0,0 +1,11 @@
+<?php
+namespace Swapnil\HelloMagento\Block;
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{
+    protected function _prepareLayout()
+    {
+
+    }
+}
